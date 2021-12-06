ad8d2b55b8dee8f3188644cc3f1d15d8b74a8789
philippmeissner <pm@zeit.dev>
2021-12-06 20:34:13 +0100

    better sizing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -12,7 +12,7 @@ const App: FunctionalComponent = () => {
   return (
     <div id="preact_root">
       {!isProduction && <Viewport/>}
-      <div className="bg-gradient-to-b from-gray-800 to-gray-900">
+      <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900">
         <Router>
           <Route path="/" component={Quote}/>
           <Route path="/impressum" component={Imprint}/>
