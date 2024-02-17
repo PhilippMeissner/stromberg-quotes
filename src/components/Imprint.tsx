595b94358aa7990bc5d5d61153a40903c59f6940
philippmeissner <philipp.meissner@dyve.agency>
2024-02-17 18:19:34 +0100

    content updates

--- src/components/Imprint.tsx
+++ src/components/Imprint.tsx
@@ -8,19 +8,29 @@ const Imprint: FunctionalComponent = () => {
     <div className={"text-white p-20 h-screen"}>
       <h1>Impressum</h1>
 
-      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG:</h2>
       <p>Philipp Mei&szlig;ner<br />
         Steinstr. 26<br />
         24118 Kiel</p>
-
-      <h2>Kontakt</h2>
+      <br />
+      <h2>Kontakt:</h2>
       <p>Telefon: 01724173337<br />
-        E-Mail: admin@philippmeissner.dev</p>
+        E-Mail: <a className="underline" href="mailto:admin@philippmeissner.dev">admin@philippmeissner.dev</a></p>
 
-      <h2>Redaktionell verantwortlich</h2>
-      <p>Die dargestellten Zitate stammen von der &ouml;ffentlichen API &quot;https://stromberg-api.de&quot;. Die Verantwortlichen f&uuml;r diese Inhalte k&ouml;nnen hier eingesehen werden: <a href="https://stromberg-api.de/Impressum">https://stromberg-api.de/Impressum</a></p>
+      <br />
+      <h2>Redaktionell verantwortlich:</h2>
+      <p>Die dargestellten Zitate stammen von der &ouml;ffentlichen API <a
+        className="underline"
+        href="https://stromberg-api.de">https://stromberg-api.de</a>.
+        Die Verantwortlichen f&uuml;r diese Inhalte k&ouml;nnen hier eingesehen werden: <a
+          className="underline"
+          href="https://stromberg-api.de/Impressum">https://stromberg-api.de/Impressum</a></p>
 
-      <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+      <br />
+      <p>Quelle: <a
+        className="underline"
+        href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a>
+      </p>
     </div>
   );
 };
