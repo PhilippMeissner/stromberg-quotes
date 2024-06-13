687002b897a9e1632e18a3bba8dfbf70868dcb77
philippmeissner <philipp.meissner@dyve.agency>
2024-06-13 23:02:25 +0200

    adds more data; removes unnecessary bracket syntax

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,9 +2,9 @@ import {FunctionalComponent, h} from 'preact';
 
 const Badge: FunctionalComponent = () => {
   return (
-    <a href={'https://dyve.agency/de'} rel={"nofollow noopener noreferrer"} target={'_blank'}>
+    <a href='https://dyve.agency/de' rel="nofollow noopener noreferrer" target='_blank'>
       <div
-        className={'bg-gradient-to-tl from-gray-700 to-gray-800 absolute top-0 right-0 bg-black p-4 text-white rotate-45 transform text-center min-w-badge translate-x-14 translate-y-10 overflow-hidden'}>
+        className='bg-gradient-to-tl from-gray-700 to-gray-800 absolute top-0 right-0 bg-black p-4 text-white rotate-45 transform text-center min-w-badge translate-x-14 translate-y-10 overflow-hidden'>
         powered by <span className="font-bold">dyve</span>
       </div>
     </a>
--- src/components/Imprint.tsx
+++ src/components/Imprint.tsx
@@ -7,19 +7,30 @@ const Imprint: FunctionalComponent = () => {
   return (
     <div className={"text-white p-20 h-screen"}>
       <h1>Impressum</h1>
+      <br />
 
       <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
       <p>Philipp Mei&szlig;ner<br />
         Steinstr. 26<br />
-        24118 Kiel</p>
+        24118 Kiel
+      </p>
+      <br />
 
       <h2>Kontakt</h2>
       <p>E-Mail: admin@philippmeissner.dev</p>
+      <p>GitHub: <a rel="nofollow noopener noreferrer" target="_blank" href="https://github.com/philippmeissner">github.com/philippmeissner</a></p>
+      <br />
 
       <h2>Redaktionell verantwortlich</h2>
-      <p>Die dargestellten Zitate stammen von der &ouml;ffentlichen API &quot;https://stromberg-api.de&quot;. Die Verantwortlichen f&uuml;r diese Inhalte k&ouml;nnen hier eingesehen werden: <a href="https://stromberg-api.de/Impressum">https://stromberg-api.de/Impressum</a></p>
+      <p>Die dargestellten Zitate stammen von der &ouml;ffentlichen API &quot;https://stromberg-api.de&quot;. Die
+        Verantwortlichen f&uuml;r diese Inhalte k&ouml;nnen hier eingesehen werden: <a
+          rel="nofollow noopener noreferrer" target="_blank"
+          href="https://stromberg-api.de/Impressum">https://stromberg-api.de/Impressum</a></p>
 
-      <p>Quelle: <a href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a></p>
+      <p>Quelle: <a
+        rel="nofollow noopener noreferrer" target="_blank"
+        href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a>
+      </p>
     </div>
   );
 };
